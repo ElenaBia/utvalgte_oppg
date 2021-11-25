45870d7df1ef94bd86d5a8ddd8c5b3e1dfed73a1
Elena Bianca Balaban <elenabiancabal@gmail.com>
2021-11-25 08:54:41 +0100

    kommentarer

--- brute_force/kryptering.js
+++ brute_force/kryptering.js
@@ -27,6 +27,14 @@ let ukryptertMelding = "";
 let krypteringsnokkel = 0; // "Forskyvningen", kvitteringsnøkkelen, eksempelvis skal 'a' bli 'c'. 0 gjer at 'a' blir 'a', 1 gjer at 'a' blir 'b'
 let karakter = "";
 
+
+/*    ------------------------------------
+
+        funksjoner som krypterer boktav, men 
+        ikke hele meldinger?
+    
+     -----------------------------------*/
+
 // Krypteringsfunksjonen, som får inn bokstaven ein skal kryptere, samt krypteringsnøkkelen. Returnerer den nye bokstaven.
 function krypterBokstav(bokstavInn, krypteringsnokkelInn){
     posisjon = alfabet.indexOf(bokstavInn);
@@ -54,37 +62,64 @@ function dekrypterBokstav(bokstavInn, krypteringsnokkelInn){
 // Test av kryptering, utan at brukar må skrive inn:
 console.log(krypterBokstav("a",1));
 
+
 /*
-    Funksjonen som går gjennom heile meldinga (frå inputfeltet) og krypterer
+
+
+    Funksjonen som går gjennom heile meldinga (frå inputfeltet) og krypterer 
+    med hjelp av krypterings funksjonen for bokstav
     ---------------------------------------------------------
+
+
+
 */
 function f_krypterMelding(){
     // Hentar ut melding
     ukryptertMelding = inputMelding.value;
     console.log(ukryptertMelding);
+
+    //forsikrer at nøkkelen er et tall
     krypteringsnokkel = Number(inputKrypteringsnokkel.value);
     console.log(krypteringsnokkel);
+
+    //lar den krypterte mld være tom for så vidt
     kryptertMelding = "";
 
-    for(let i = 0; i < ukryptertMelding.length; i++) { // For heile meldinga/teksten
-        if(alfabet.indexOf(ukryptertMelding.charAt(i)) !== -1){ // Dersom me finn bokstaven i alfabetet...
-            kryptertMelding = kryptertMelding + krypterBokstav(ukryptertMelding.charAt(i),krypteringsnokkel); // ...send den til kryptering
+    //Loopen skal gå gjennom hele meldingen i input feltet
+    for(let i = 0; i < ukryptertMelding.length; i++) { 
+
+        // Dersom vi finner bokstaven i alfabetet...
+        if(alfabet.indexOf(ukryptertMelding.charAt(i)) !== -1){ 
+            //skal den bli kryptert.
+            kryptertMelding = kryptertMelding + krypterBokstav(ukryptertMelding.charAt(i),krypteringsnokkel); 
             //console.log("If, bokstav funnen. Iterasjon nr. " + i + ". Innhald ukryptertMelding.charAt(i) = " + ukryptertMelding.charAt(i));
         }
         else{
-            kryptertMelding = kryptertMelding + ukryptertMelding.charAt(i); // Dersom me ikkje finn bokstaven/teiknet, legg det til slik det er.
+            // Dersom vi ikkje finner bokstaven/teiknet, legg det til slik det er.
+            kryptertMelding = kryptertMelding + ukryptertMelding.charAt(i); 
             //console.log("Else, bokstav ikkje funnen. Iterasjon nr. " + i + ". Innhald: " + ukryptertMelding.charAt(i));
         }
     }
+
+
     console.log("Resultatet med 'tradisjonell for-løkke' er = " + kryptertMelding + ".");
     outputMelding.innerHTML = "Kryptert melding av input <span style='color:red;'>" + ukryptertMelding + "</span>, blir oversatt til <span style='color:red;'>" + kryptertMelding + "</span>.";
 
-    // NB: Alternativ for .. of - med same resultat. Kan kuttast ut.
+
+
+    /*
+
+        Alternatic som er mindre komplisert
+        ----------------------------------
+    */
     let resultat = "";
     for(let bokstav of ukryptertMelding) {
+
+        //hvis den er i alfabetet
         if(alfabet.indexOf(bokstav) !== -1){
             resultat = resultat + krypterBokstav(bokstav,krypteringsnokkel);
         }
+        //hvis den er innenfor
         else{
             resultat = resultat + bokstav;
         }
@@ -92,19 +127,32 @@ function f_krypterMelding(){
     console.log("Resultatet med for .. of er: " + resultat);
 }
 
+
+
+
+
 /* 
+
+
     Funksjonen som dekypterer meldinga frå tekstfeltet
     --------------------------------------------------
+
+
 */
 function f_dekrypterMelding(){
+    //setter tom verdi for resultatet til å begynne med
     dekryptertMelding = "";
+    //henter info fra input feltet
     kryptertmelding = inputMelding.value;
     krypteringsnokkel = Number(inputKrypteringsnokkel.value);
 
     for(let bokstav of kryptertmelding){
+
+        //hvis den er innenfor alfabetet
         if(alfabet.indexOf(bokstav) !== -1){
             dekryptertMelding = dekryptertMelding + dekrypterBokstav(bokstav, krypteringsnokkel);
         }
+        //hvis den er utenfor, skal den bare bli seg selv
         else{
             dekryptertMelding = dekryptertMelding + bokstav;
         }
@@ -119,20 +167,34 @@ function f_dekrypterMelding(){
     ----------------------------------------------------------------------------------------
 */
 function f_bruteForce(){
+    //henter ut kryptert melding fra inoutfeltet
     kryptertMelding = inputMelding.value;
     dekryptertMelding = "";
-    krypteringsnokkel = 0; // Denne tek for-løkka og endrar fortløpande, 'krypteringsnokkel = j'
+
+    //skal endres av for løkken og bli til j
+    krypteringsnokkel = 0; 
+
     outputBruteForce.innerHTML = "";
 
+    //j skal øke med 1 for hele lengden av alfabetet
     for(let j = 1; j<=alfabet.length; j++){
+
+        //sier først hva nøkkelen blir for hver gang den øker
         outputBruteForce.innerHTML += "<p>Nøkkel: " + j + "<br>";
+
+        //setter ny tom verdi..hvorfor?
         dekryptertMelding = "";
+
         for(let bokstav of kryptertMelding){
+            //hva skjer hvis nøkkelen er innenfor alfabetet
             if(alfabet.indexOf(bokstav) !== -1){
+                //setter krypteringsnøkkelen til j
                 krypteringsnokkel = j;
                 dekryptertMelding = dekryptertMelding + dekrypterBokstav(bokstav,krypteringsnokkel);
+                //  dekryptertMelding += dekryptertBokstav(bokstav, nøkkel...) alternativt?
             }
             else{
+                //hvis ikke bare sett bokstaven til det samme
                 dekryptertMelding = dekryptertMelding + bokstav;
             }
         }
